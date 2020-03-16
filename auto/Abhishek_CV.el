f8e025182abebf6ca37912ffcf88e05be00cbfa8
Abhishek Pandey <abhiganit@gmail.com>
2020-03-16 19:51:05 -0400

    checking

--- auto/Abhishek_CV.el
+++ auto/Abhishek_CV.el
@@ -5,12 +5,13 @@
                      '(("res" "margin" "line" "pifont" "palatino" "courier")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("hyperref" "hidelinks")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -24,6 +25,5 @@
     '("doi" 1)
     '("doilink" 1))
    (LaTeX-add-environments
-    "list"))
- :latex)
+    "list")))
 
